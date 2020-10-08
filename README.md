442252986e98b4c6ba65214367e872dfe1d3fa90
Yang Liu <33103855+Enderlogic@users.noreply.github.com>
2020-10-08 13:35:15 +0800

    v1.0

---
+++ README.md
@@ -0,0 +1,28 @@
+# Introduction
+This is an implementation of the Redundant Edge Detection (RED) algorithm which aims to eliminate the redundant edges learned by structure learning algorithms in the presence of measurement error.
+
+There are two embedded structure learning algorithms (HC, PC-Stable) in this code. If you want to test other algorithms, please save the learned graph in the folder "learned graph" with format as the files in it.
+
+The sturcture of a BN is stored in an object of dictionary. The key of the dictionary is the name of variable in BN and the corresponding value is a dictionary with two lists ("par" and "nei") consisted by the key's parents and neighbors respectively. The parent of a variable will only appear in the "par" list but not in the "nei" list. 
+# Requirements
+- Python 3.5+
+- pandas
+- scipy
+- numba
+
+# Contents
+- folders
+    - true graph: the ground truth graph
+    - learned graph: the graphs learned by structure learning algorithms
+    - corrected graph: the graphs corrected by RED
+    - result: the evaluation results of learned graph and corrected graph
+    - lib: a folder with necessary scripts including:
+        - accessory.py: some auxiliary functions used in other scripts
+        - evaluation.py: the F1 evaluation
+        - score.py: some score computing functions used in other scripts
+        - hc.py: implementation of the hill-climbing algorithm
+        - pc_stable.py: implementation of the PC-Stable algorithm
+        - red.py: implementation of the RED algorithm
+- RED_demo.py: a demo sript
+
+# Redundant-Edge-Detection
---
+++ RED_demo.py
@@ -0,0 +1,66 @@
+import pandas as pd
+from lib.evaluation import compare, shd
+from lib.accessory import data_preprocessing, cpdag
+import json
+from lib.pc_stable import pc_stable
+from lib.hc import hc
+from lib.red import red
+from pathlib import Path
+import os
+import csv
+
+dataset_repository = ['asia', 'alarm', 'child', 'insurance', 'mildew', 'water', 'hailfinder']
+datasize_repository = [100, 500, 1000, 5000, 10000, 50000, 100000]
+noise_repository = ['E0', 'E5', 'E10']
+algorithm_repository = ['hc', 'pc.stable', 'h2pc', 'mmhc', 'gobnilp']
+
+dataset_list = ['asia', 'alarm', 'child', 'insurance', 'mildew', 'water', 'hailfinder']
+datasize_list = [100, 500, 1000, 5000, 10000, 50000, 100000]
+noise_list = ['E0', 'E5', 'E10']
+algorithm_list = ['hc', 'h2pc', 'pc.stable', 'gobnilp']
+
+result_columns = ['dataset', 'datasize', 'noise', 'algorithm', 'f1', 'shd']
+result_complete = pd.read_csv('result/result_complete.csv')
+result = []
+for dataset in dataset_list:
+    # load true graph
+    with open('true graph/' + dataset + '.json') as json_file:
+        true_dag = json.load(json_file)
+    true_cpdag = cpdag(true_dag)
+    for datasize in datasize_list:
+        for noise in noise_list:
+            if noise == 'N':
+                noise_in_file = 'Error-free'
+            else:
+                noise_in_file = 'Noisy'
+            # load data
+            data_file = 'data/' + dataset + '/' + noise + '/' + dataset + str(datasize) + noise + '.csv'
+            if os.path.exists(data_file):
+                data_training = pd.read_csv(data_file)
+                data, counts, arities, arities_wm, varnames = data_preprocessing(data_training)
+                for alg in algorithm_list:
+                    learned_dag_file = 'learned graph/' + alg + '/' + dataset + '/' + noise + '/' + dataset + str(datasize) + noise + '.json'
+                    if os.path.exists(learned_dag_file):
+                        with open(learned_dag_file) as json_file:
+                            learned_dag = json.load(json_file)
+                        # evaluate learned graph
+                        result.append({'dataset': dataset, 'datasize': datasize, 'noise': noise_in_file, 'algorithm': alg, 'f1': compare(true_cpdag, cpdag(learned_dag))['f1'], 'shd': shd(true_cpdag, cpdag(learned_dag))})
+                        print(result[-1])
+                        # run RED algorithm
+                        modified_cpdag = red(learned_dag, data, counts, arities, varnames, score_function = 'bic')
+                        # generate and save modified graph
+                        modified_folder = 'modified graph/' + alg + '/' + dataset + '/' + noise
+                        Path(modified_folder).mkdir(parents=True, exist_ok=True)
+                        with open(modified_folder + '/' + dataset + str(datasize) + noise + '_modified.json', 'w') as outfile:
+                            json.dump(modified_cpdag, outfile)
+                        # or load modified graph from existing file
+                        # with open('modified graph/' + alg + '/' + dataset + '/' + noise + '/' + dataset + str(datasize) + noise + '_corrected.json') as json_file:
+                        #     corrected_cpdag = json.load(json_file)
+                        # evaluate modified graph
+                        result.append({'dataset': dataset, 'datasize': datasize, 'noise': noise_in_file, 'algorithm': alg + '+red', 'f1': compare(true_cpdag, cpdag(modified_cpdag))['f1'], 'shd': shd(true_cpdag, cpdag(modified_cpdag))})
+                        print(result[-1])
+with open('result/results.csv', 'w') as csvfile:
+    writer = csv.DictWriter(csvfile, fieldnames=result_columns)
+    writer.writeheader()
+    for row in result:
+        writer.writerow(row)
